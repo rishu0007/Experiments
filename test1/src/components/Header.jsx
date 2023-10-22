95d5aca6b5c70009434dc25e78475d3f90f12754
Rishu <kumarrishu571@gmail.com>
2023-10-23 01:12:34 +0530

    test1

---
+++ test1/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/Header.css'
+
+const Header = () => {
+  return (
+    <>
+        <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        </nav>
+    </>
+  )
+}
+
+export default Header
